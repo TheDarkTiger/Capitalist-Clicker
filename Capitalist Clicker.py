f1b8703287da84e88e5bdd1a0855f5bb9278a1a1
TheDarkTiger <thedarktiger@hotmail.com>
2022-04-17 01:23:10 +0200

    Add 'ka1' font, Update task draw

--- Capitalist Clicker.py
+++ Capitalist Clicker.py
@@ -26,25 +26,37 @@ c_blue = (68,133,207)
 c_lblue = (125,187,255)
 
 
-# Draw a task
-def draw_task( color=(255,0,255), y=30 ) :
-	pygame.draw.circle( screen, color, (30,y), 20,5 )
-	pygame.draw.rect( screen, color, (70,y-15, 200,30) )
-	pygame.draw.rect( screen, (0,0,0), (75,y-10, 190,20) )
-	
-
-
 # Config window
 pygame.init()
 screen = pygame.display.set_mode( [300,450] )
 pygame.display.set_caption( "Capitalist Clicker" )
 framerate = 60
 timer = pygame.time.Clock()
-font = pygame.font.Font( "freesansbold.ttf", 16 )
+font = pygame.font.Font( "ka1.ttf", 16 )
+
+
+# Game variables
+tasks = {}
+tasks["green"] = {"color":c_lgreen, "y":50, "value":1}
+tasks["red"] = {"color":c_red, "y":110, "value":2}
+tasks["orange"] = {"color":c_orange, "y":170, "value":3}
+tasks["white"] = {"color":c_gray, "y":230, "value":4}
+tasks["purple"] = {"color":c_purple, "y":290, "value":5}
+
+
+# Draw a task
+def draw_task( color=(255,0,255), y=30, value=0 ) :
+	pygame.draw.circle( screen, color, (30,y), 20,5 )
+	pygame.draw.rect( screen, color, (70,y-15, 200,30) )
+	pygame.draw.rect( screen, (0,0,0), (75,y-10, 190,20) )
+	
+	valueTextCanvas = font.render( str( value ), True, c_white )
+	screen.blit( valueTextCanvas, (16, y-10) )
+	
 
 # Main
 def main() :
-	
+		
 	# Set the game
 	backgroundColor = c_black
 	running = True
@@ -59,11 +71,8 @@ def main() :
 		# Drawing
 		screen.fill( backgroundColor )
 		
-		draw_task( c_lgreen, 50 )
-		draw_task( c_red, 110 )
-		draw_task( c_orange, 170 )
-		draw_task( c_gray, 230 )
-		draw_task( c_purple, 290 )
+		for task in tasks :
+			draw_task( tasks[task]["color"], tasks[task]["y"], tasks[task]["value"] )
 		
 		pygame.display.flip()
 		
