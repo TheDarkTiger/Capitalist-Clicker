324d859dbfd1bdaa79997e52cf248d01ea83dbe2
TheDarkTiger <thedarktiger@hotmail.com>
2022-05-14 21:46:08 +0200

    Add working task and display

--- Capitalist Clicker.py
+++ Capitalist Clicker.py
@@ -9,14 +9,32 @@ import pygame.freetype
 
 
 # Draw a task
-def draw_task( color=(255,0,255), y=30, value=0 ) :
-	pygame.draw.circle( screen, color, (30,y), 20,5 )
-	pygame.draw.rect( screen, color, (70,y-15, 200,30) )
-	pygame.draw.rect( screen, (0,0,0), (75,y-10, 190,20) )
-	
+def draw_task( color=(255,0,255), y=30, value=0, advancement=0 ) :
+	# Icon
+	icon = pygame.draw.circle( screen, color, (30,y), 20,5 )
 	valueTextCanvas = font.render( str( value ), True, c_white )
 	screen.blit( valueTextCanvas, (16, y-10) )
 	
+	# Bar
+	pygame.draw.rect( screen, color, (70,y-15, 200,30) )
+	pc = (advancement/100.0)
+	pygame.draw.rect( screen, (0,0,0), (75+(190*pc),y-10, 190*(1-pc),20) )
+	
+	return icon
+
+# Update a task
+def update_task( value=0, working=False, advancement=0, speed=0 ):
+	global score
+	
+	if working :
+		advancement += speed
+	
+	if advancement >= 100 :
+		advancement = 0
+		working = False
+		score += value
+	
+	return working, advancement
 
 # Pico8ish palette
 c_white = (255,255,255)
@@ -44,15 +62,16 @@ pygame.display.set_caption( "Capitalist Clicker" )
 framerate = 60
 timer = pygame.time.Clock()
 font = pygame.font.Font( "ka1.ttf", 16 )
+score = 0
 
 
 # Game variables
 tasks = {}
-tasks["green"] = {"color":c_lgreen, "y":50, "value":1}
-tasks["red"] = {"color":c_red, "y":110, "value":2}
-tasks["orange"] = {"color":c_orange, "y":170, "value":3}
-tasks["white"] = {"color":c_gray, "y":230, "value":4}
-tasks["purple"] = {"color":c_purple, "y":290, "value":5}
+tasks["green"] = {"color":c_lgreen, "y":50, "value":1, "working":False, "speed":5, "advancement":0, "icon":None}
+tasks["red"] = {"color":c_red, "y":110, "value":2, "working":False, "speed":4, "advancement":0, "icon":None}
+tasks["orange"] = {"color":c_orange, "y":170, "value":3, "working":False, "speed":3, "advancement":0, "icon":None}
+tasks["white"] = {"color":c_gray, "y":230, "value":4, "working":False, "speed":2, "advancement":0, "icon":None}
+tasks["purple"] = {"color":c_purple, "y":290, "value":5, "working":False, "speed":1, "advancement":0, "icon":None}
 
 
 # Main
@@ -67,12 +86,20 @@ while running :
 	for event in pygame.event.get() :
 		if event.type == pygame.QUIT :
 			running = False
+		
+		if event.type == pygame.MOUSEBUTTONDOWN :
+			for task in tasks :
+				if tasks[task]["icon"] != None :
+					if tasks[task]["icon"].collidepoint( event.pos ) :
+						tasks[task]["working"] = True
 	
 	# Drawing
 	screen.fill( backgroundColor )
 	
 	for task in tasks :
-		draw_task( tasks[task]["color"], tasks[task]["y"], tasks[task]["value"] )
+		t = tasks[task]
+		t["working"], t["advancement"] = update_task( t["value"], t["working"], t["advancement"], t["speed"] )
+		t["icon"] = draw_task( t["color"], t["y"], t["value"], t["advancement"] )
 	
 	pygame.display.flip()
 	
